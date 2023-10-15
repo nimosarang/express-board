48cf0ea7a96c3d09e8dd27dab19b5f64f97da4a3
nimosarang <111734043+nimosarang@users.noreply.github.com>
2023-10-15 23:27:40 +0900

    project setting and test

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const app = express();
+
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    title: "안녕하세요",
+    message: "만나서 반갑습니다!",
+  });
+});
+
+app.listen(3000);
